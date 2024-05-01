cba123051ff6fbf10135bbe1cd4f04aa56ccc8fd
evaaaanwarr <evaaanwarrr@gmail.com>
2024-05-01 20:14:11 +0800

    delete

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-echo "Applying migrations..."
-python manage.py migrate --noinput
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-echo "Starting server..."
-gunicorn epaa_site.wsgi
-exec "$@"
