d4286d977dfc4f579419280db7b1736f5f1e1ba7
evaaaanwarr <evaaanwarrr@gmail.com>
2024-05-03 08:33:43 +0800

    initial commit

--- epaa_site/urls.py
+++ epaa_site/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('epaa_site.urls')),
+    path('', include('fashionflow.urls')),
 ]
 
 if settings.DEBUG:
