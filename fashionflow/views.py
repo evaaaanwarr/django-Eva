64dd2b604c847b201419f9b0c0c7aa04d205d268
evaaaanwarr <evaaanwarrr@gmail.com>
2024-05-04 14:57:25 +0800

    Update

--- fashionflow/views.py
+++ fashionflow/views.py
@@ -9,9 +9,9 @@ def home(request):
     posts = Blog.objects.filter(status=1)
     return render(request, 'home.html', {'categories': categories, 'pages': pages, 'posts': posts})
 
-def category_list(request):
+def category(request):
     categories = Category.objects.all()
-    return render(request, 'category_list.html', {'categories': categories})
+    return render(request, 'category.html', {'categories': categories})
 
 def post_list(request):
     posts = Blog.objects.filter(status=1)
@@ -21,9 +21,9 @@ def page_list(request):
     pages = Page.objects.filter(is_published=True)
     return render(request, 'page_list.html', {'pages': pages})
 
-def post_detail(request, slug):
+def blog(request, slug):
     post = get_object_or_404(Blog, slug=slug, status=1)
-    return render(request, 'post_detail.html', {'post': post})
+    return render(request, 'blog.html', {'post': post})
 
 def page_detail(request, slug):
     page = get_object_or_404(Page, slug=slug, is_published=True)
